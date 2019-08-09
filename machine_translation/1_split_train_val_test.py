c8f7219c9cc991f026854f977c9068cbb2ba7bbd
xiaotaw <xiaotaw@qq.com>
2019-08-09 17:16:16 +0800

    MachineTranslation: 'split_corpus' support cmd usage

--- machine_translation/1_split_train_val_test.py
+++ machine_translation/1_split_train_val_test.py
@@ -1,10 +1,39 @@
-# 
+#!/usr/bin/env python3 
+import argparse
+import codecs
+import logging
+import os
 import sys
-import nunpy as np
+import numpy as np
+from collections import namedtuple
 
-#assert (sys.version_info.major == 2), u"python version"
+#assert (sys.version_info.major == 3), u"python version"
 
-def split_corpus(corpus, val=100, test=500):
+def parse_args(args=None):
+    parser = argparse.ArgumentParser(
+        description = "Split corpus into train, val, and test",
+        usage = __file__ + " [<args>] [-h | --help]"
+    )
+    
+    parser.add_argument("--filenames", type=str, nargs="+", required=True, 
+        metavar="FILENAME", help="Path of corpus")
+    parser.add_argument("--n_val", type=int, default=100, 
+        metavar="NUM_VAL", help="Number of val")
+    parser.add_argument("--n_test", type=int, default=500, 
+        metavar="NUM_VAL", help="Number of test")
+    parser.add_argument("--fn_format", type=str, 
+        metavar="[PREFIX]{TYPE}_{BASENAME}.{EXTENSION}",
+        default="{prefix}{type}_{basename}.{extension}", 
+        help="output filename format")
+    parser.add_argument("--prefix", type=str, default="")
+    parser.add_argument("--basename", type=str, default="",
+        help="if not defined, use the basename of the input filename")
+    parser.add_argument("--extension", type=str, default="",
+        help="if not defined, use the extension of the input filename")
+    
+    return parser.parse_args(args)
+
+def split_corpus(corpus, n_val, n_test):
     assert isinstance(corpus, list)
     l = len(corpus)
     np.random.seed(0)
@@ -12,11 +41,50 @@ def split_corpus(corpus, val=100, test=500):
     np.random.shuffle(m)
     val_lst, test_lst, train_lst = [], [], []
     for i, v in enumerate(m):
-        if i < val:
+        if i < n_val:
             val_lst.append(corpus[m[v]])
-        elif i < val + test:
+        elif i < n_val + n_test:
             test_lst.append(corpus[m[v]])
         else:
             train_lst.append(corpus[m[v]])
     return val_lst, test_lst, train_lst
+
+if __name__ == "__main__":
+    args = parse_args()
+    logging.basicConfig(level=logging.INFO, format="[%(levelname)s] %(message)s")
     
+    for fn in args.filenames:
+        f = codecs.open(fn, encoding="utf8")
+        s = f.readlines()
+        f.close()
+        logging.info("Read %s, total lines %d ..." % (fn, len(s)))
+        
+        val, test, train = split_corpus(s, args.n_val, args.n_test)
+        logging.info("Splited, val: %d, test: %d, train: %d" % (len(val), 
+            len(test), len(train)))
+            
+        filepath, filename = os.path.split(fn)
+        basename, extension = os.path.splitext(filename)
+        extension = extension.lstrip(".")
+        if not args.basename:
+            args.basename = basename 
+        if not args.extension:
+            args.extension = extension
+
+        fn_format = args.fn_format.format(prefix=args.prefix, type="%s", 
+            basename=args.basename, extension=args.extension)
+        
+        if len(val) > 0:
+            f = codecs.open(fn_format % "var", "w", encoding="utf8")
+            _ = f.write("".join(val))
+            f.close()
+        if len(test) > 0:
+            f = codecs.open(fn_format % "test", "w", encoding="utf8")
+            _ = f.write("".join(test))
+            f.close()
+        if len(train) > 0:
+            f = codecs.open(fn_format % "train", "w", encoding="utf8")
+            _ = f.write("".join(train))
+            f.close()
+
+
