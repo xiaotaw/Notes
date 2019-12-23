524c5fb4fa1b02d01593c0578b7ba987e2bc6ef2
xiaotaw <xiaotaw@qq.com>
2019-12-23 17:33:58 +0800

    tensorflow: 增加对tensorflow=2.0动态库使用的测试

--- tensorflow/test_c/run.sh
+++ tensorflow/test_c/run.sh
@@ -1,13 +1,41 @@
 #!/usr/bin/env bash
 
+case $1 in
+    r1.8)
+        echo "use tensorflow r.8"
+        TF_HOME=../release_c/r1.8/
+        TF_INC=-I$TF_HOME/inc/
+        TF_LIB=$TF_HOME/lib/linux64_cuda10.0_cudnn7.6_glibc2.12/
+        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$TF_LIB 
+        linkopt="-lrt -lpthread -ltensorflow_cc -ltensorflow_framework"
+        ;;
+    r2.0)    
+        echo "use tensorflow r2.0"
+        TF_HOME=../release_c/r2.0/
+        #TF_INC="-I$TF_HOME/inc/"
+        TF_INC="-I$TF_HOME/inc/ \
+                -I$TF_HOME/inc/external/eigen_archive/ \
+                -I$TF_HOME/inc/external/com_google_absl \
+                -I$TF_HOME/inc/external/com_google_protobuf/src"
+        TF_LIB=$TF_HOME/lib/linux64_cuda10.0_cudnn7.6_glibc2.12_glibcxx3.4.22/
+        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$TF_LIB 
+        linkopt="-lrt -lpthread -ltensorflow_cc"
+        ;;
+    *)       # unknown version, use r.18
+        echo "unknown tf version, use default tensorflow_version = r1.8"
+        TF_HOME=../release_c/r1.8/
+        TF_INC=-I$TF_HOME/inc/
+        TF_LIB=$TF_HOME/lib/linux64_cuda10.0_cudnn7.6_glibc2.12/
+        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$TF_LIB 
+        linkopt="-lrt -lpthread -ltensorflow_cc -ltensorflow_framework"
+        ;;
+esac
 
-TF_HOME=../release/r1.8/
-TF_INC=$TF_HOME/inc/
-TF_LIB=$TF_HOME/lib/linux64_cuda10.0_cudnn7.6_glibc2.12/
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$TF_LIB 
 
-
-
+#TF_HOME=../release_c/r1.8/
+#TF_INC=$TF_HOME/inc/
+#TF_LIB=$TF_HOME/lib/linux64_cuda10.0_cudnn7.6_glibc2.12/
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$TF_LIB 
 
 
 
@@ -17,9 +45,9 @@ for target in ${target_lst[@]}; do
     echo -e "\n**************** compile $target ... ****************"
     g++ -std=c++0x -o $target \
         -g -Wall -D_DEBUG -Wshadow -Wno-sign-compare -w \
-        -I$TF_INC \
+        $TF_INC \
         -L$TF_LIB \
-        -lrt -lpthread -ltensorflow_cc -ltensorflow_framework \
+        $linkopt \
         $target.c
 
     echo -e "\n**************** run $target ... ****************"
